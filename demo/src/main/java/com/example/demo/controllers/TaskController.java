773d00440fbe4154a710e59271dff46a5d2248e0
GeorgeMirror <51678325+GeorgeMirror@users.noreply.github.com>
2022-09-09 21:28:21 +0300

    Update TaskController.java
    Добавлена защита от параллельного изменения

--- demo/src/main/java/com/example/demo/controllers/TaskController.java
+++ demo/src/main/java/com/example/demo/controllers/TaskController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.*;
 public class TaskController {
     @Autowired
     TaskService taskService;
+    private volatile boolean f = true;
     @GetMapping("/list")
     public Object modeList(){
         return taskService.modeList();
-
     }
     @GetMapping("/h2")
     public Object modeH2(){
@@ -21,38 +21,116 @@ public class TaskController {
     }
 
     @PostMapping
-    public Object createTask(@RequestBody final Task task){
-        return taskService.create(task);
+    public Object createTask(@ModelAttribute("Task") final Task task){
+        while (!f){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        f = false;
+        Object res =  taskService.create(task);
+        f = true;
+        return res;
     }
 
     @GetMapping("/{id}")
     public Object show(@PathVariable("id") int id) {
-        return taskService.show(id);
+        while (!f){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        f = false;
+        Object res = taskService.show(id);
+        f = true;
+        return res;
     }
     @GetMapping
     public Object show() {
-        return taskService.show();
+        while (!f){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        f = false;
+        Object res = taskService.show();
+        f = true;
+        return res;
     }
     @DeleteMapping("/{id}")
     public Object delete(@PathVariable("id") int id) {
-        return taskService.delete(id);
+        while (!f){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        f = false;
+        Object res = taskService.delete(id);
+        f = true;
+        return res;
     }
     @PatchMapping("/{id}")
-    public Object update(@ModelAttribute("task") Task task){
-        return taskService.update(task);
+    public Object update(@ModelAttribute("Task") Task task){
+        while (!f){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        f = false;
+        Object res = taskService.update(task);
+        f = true;
+        return res;
     }
     @GetMapping("/produce")
     public Object produce(){
+        while (!f){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        f = false;
         taskService.produce();
+        f = true;
         return "ок";
     }
     @GetMapping("/produce_{i}")
     public Object produce(@PathVariable("i") int i){
-        return taskService.produce(i);
+        while (!f){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        f = false;
+        Object res = taskService.produce(i);
+        f = true;
+        return res;
     }
 
     @GetMapping("/start")
     public void start(){
+        while (!f){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        f = false;
         taskService.start();
+        f = true;
     }
 }
